0d4488b9a808e6287c5c771aae26f469a355fe0f
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2022-01-01 17:29:50 -0600

    worked more on what was needed for core server file. created routes folder with api and homeRoutes files. commented more on server file

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
+//to log responses to the console for easier debugging
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
-
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger("dev")); //sets morgan to developer mode
 
 app.use(express.urlencoded({
     extended: true
@@ -17,16 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", {
-    useNewUrlParser: true
+app.use(require("./routes/api.js"));
+
+app.use(require("./routes/homeRoutes.js"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+    useNewUrlParser: true, // since MDB driver deprecated their connection string parser, this flag allows use of old parser
+    useFindAndModify: true, //If multiple docs match, choose the first one in the specified sort order as the object to manipulate.
+    useUnifiedTopology: true, //opt in to using the MongoDB driver's new connection management engine. make Mongoose's default index build use createIndex() instead of ensureIndex() to avoid deprecation warnings
+
+
 });
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
