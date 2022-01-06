b9d597cf2ece9d7f70eb281813d72816da191c79
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2022-01-05 18:24:45 -0600

    fixing seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/fitness-tracker', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
